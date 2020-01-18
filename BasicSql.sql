b2856a8f55603a0abdc2b9fe5f46440906926806
yogii1981 <yogeshsharma1981@gmail.com>
2020-01-18 15:05:44 -0500

    Update BasicSql.sql

--- BasicSql.sql
+++ BasicSql.sql
@@ -1,4 +1,4 @@
-Select occurred_at, account_id, channel
+select occurred_at, account_id, channel
 from web_events
 limit 15;
 
@@ -9,11 +9,11 @@ from orders
 order by occurred_at
 limit 1000;
 
-
 # query to return earliest 10 orders #
 select id, occurred_at, total_amt_usd
 from orders
 limit 10;
+
 
 # Write a query to return the top 5 orders in terms of #
 #largest total_amt_usd. Include the id, account_id,   #
@@ -37,26 +37,6 @@ limit 20  ;
 #  account ID, and total dollar amount for all the orders,
 # sorted first by the account ID (in ascending order), and then
 # by the total dollar amount (in descending order).
-
-
-# IN Function usage / Comparasion operator
-
-# 1. Use the accounts table to find the account name,
-#  primary_poc, and sales_rep_id for Walmart, Target,
-#  and Nordstrom.
-
-select name, primary_poc, sales_rep_id
-from accounts
-where name in ('Walmart', 'Target','Nordstrom');
-
-
-# 2. Use the web_events table to find all information
-#  regarding individuals who were contacted via the channel
-#  of organic or adwords.
-
-select * from web_events
-where channel in ('organic','adwords');
-
 
 Select id, account_id, total_amt_usd
 from orders
@@ -99,11 +79,9 @@ from accounts
 where name like 'Exxon Mobil';
 
 
-
-
-# Create a column that divides the standard_amt_usd by 
+# Create a column that divides the standard_amt_usd by
 #the standard_qty to find the unit price for standard paper
-#  for each order. Limit the results to the first 10 orders, 
+#  for each order. Limit the results to the first 10 orders,
 # and include the id and account_id fields.
 
 select standard_amt_usd / standard_qty as unit_price, id, account_id
@@ -111,23 +89,21 @@ from orders
 limit 10;
 
 # Write a query that finds the percentage of revenue that comes
-#  from poster paper for each order. 
-# You will need to use only the columns that end with _usd. 
-# (Try to do this without using the total column.) Display the id 
+#  from poster paper for each order.
+# You will need to use only the columns that end with _usd.
+# (Try to do this without using the total column.) Display the id
 # and account_id fields also. NOTE - you will receive an error with
 # the correct solution to this question. This occurs because at
-#  least one of the values in the data creates a division by zero 
+#  least one of the values in the data creates a division by zero
 # in your formula. You will learn later in the course how to fully
-#  handle this issue. For now, you can just limit your calculations 
+#  handle this issue. For now, you can just limit your calculations
 # to the first 10 orders, as we did in question #1, and you'll avoid
 #  that set of data that causes the problem.
 
-SELECT id, account_id, 
+SELECT id, account_id,
    poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_per
 FROM orders
 LIMIT 10;
-
-
 
 # Questions using the LIKE operator
 # Use the accounts table to find
@@ -138,7 +114,7 @@ Select name
 from accounts
 where name like 'C%' ;
 
-# 2. All companies whose names contain the string 'one' 
+# 2. All companies whose names contain the string 'one'
 #  somewhere in the name.
 
 Select name
@@ -149,3 +125,22 @@ where name like '%one%' ;
 Select name
 from accounts
 where name like '%s' ;
+
+# IN Function usage / Comparasion operator
+
+# 1. Use the accounts table to find the account name,
+#  primary_poc, and sales_rep_id for Walmart, Target,
+#  and Nordstrom.
+
+select name, primary_poc, sales_rep_id
+from accounts
+where name in ('Walmart', 'Target','Nordstrom');
+
+
+# 2. Use the web_events table to find all information
+#  regarding individuals who were contacted via the channel
+#  of organic or adwords.
+
+select * from web_events
+where channel in ('organic','adwords');
+
