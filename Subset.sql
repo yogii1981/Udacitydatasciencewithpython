313768c9c34386068c35fa969a9c08462d6ee144
yogii1981 <yogeshsharma1981@gmail.com>
2020-02-04 14:00:52 -0500

    Update Subset.sql

--- Subset.sql
+++ Subset.sql
@@ -37,7 +37,13 @@ where Date_Trunc('month', occurred_at) =
 
                    
 # The average amount of standard paper sold on the first month 
- # that any order was placed in the orders table (in terms of quantity).         
+ # that any order was placed in the orders table (in terms of quantity).    
+ 
+ #The average amount of gloss paper sold on the first month that any order was placed in the orders table (in terms of quantity).
+
+#The average amount of poster paper sold on the first month that any order was placed in the orders table (in terms of quantity).
+
+#The total amount spent on all orders on the first month that any order was placed in the orders table (in terms of usd).
                    
 SELECT AVG(standard_qty) avg_std, AVG(gloss_qty) avg_gls, AVG(poster_qty) avg_pst
 FROM orders
@@ -48,3 +54,4 @@ SELECT SUM(total_amt_usd)
 FROM orders
 WHERE DATE_TRUNC('month', occurred_at) = 
       (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+                         
