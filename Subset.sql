0ad68d0e2c34329be5197d5471a0707fbe9df1fa
yogii1981 <yogeshsharma1981@gmail.com>
2020-02-04 13:58:51 -0500

    Update Subset.sql

--- Subset.sql
+++ Subset.sql
@@ -25,3 +25,26 @@ FROM (SELECT DATE_TRUNC('day',occurred_at) AS day,
       GROUP BY 1,2) sub
 GROUP BY channel
 ORDER BY 2 DESC;
+
+
+# What was the month/year combo for the first order placed?
+Select * 
+from orders
+where Date_Trunc('month', occurred_at) =
+(select Date_trunc('month',min(occurred_at)) as min_month
+ from orders)
+ order by occurred_at
+
+                   
+# The average amount of standard paper sold on the first month 
+ # that any order was placed in the orders table (in terms of quantity).         
+                   
+SELECT AVG(standard_qty) avg_std, AVG(gloss_qty) avg_gls, AVG(poster_qty) avg_pst
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+     (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+
+SELECT SUM(total_amt_usd)
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+      (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
