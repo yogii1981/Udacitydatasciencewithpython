70e83694c055fd11379bf1fa2c31a20331030f27
yogii1981 <yogeshsharma1981@gmail.com>
2020-02-29 17:04:53 -0500

    Update mode.sql

--- mode.sql
+++ mode.sql
@@ -25,3 +25,19 @@ group by account_id;
 # The following code will provide a count of all of rows in which the high column is not null.
 SELECT COUNT(high)
   FROM tutorial.aapl_historical_stock_price
+  
+  
+  
+ # query to count non-numerical data   
+SELECT COUNT(date)
+FROM tutorial.aapl_historical_stock_price
+
+# Write a query that determines counts of every single column. Which column has the most null values?
+SELECT COUNT(year) AS year,
+       COUNT(month) AS month,
+       COUNT(open) AS open,
+       COUNT(high) AS high,
+       COUNT(low) AS low,
+       COUNT(close) AS close,
+       COUNT(volume) AS volume
+  FROM tutorial.aapl_historical_stock_price
